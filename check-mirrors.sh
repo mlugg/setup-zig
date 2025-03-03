5a672dee1a157b191cf5763f8a1af0b69ea37fcd
jiacai2050 <dev@liujiacai.net>
2025-03-03 19:29:15 +0800

    add check summary

--- check-mirrors.sh
+++ check-mirrors.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 temp_path="$(mktemp)"
-
+summary_file=${GITHUB_STEP_SUMMARY:-/tmp/check-mirror-summary.md}
+time_file="$(mktemp)"
 zig_version="$(curl -sSL 'https://ziglang.org/download/index.json' | jq -r '.master.version')"
 filename="zig-linux-x86_64-$zig_version.tar.xz"
 
 curl -L "https://ziglang.org/builds/$filename" >"$temp_path"
 correct_sum="$(sha512sum "$temp_path")"
 
+echo "| Mirror | URL | Cost(seconds) |" >> "${summary_file}"
+echo "|:-:|:-:|:-:|" >> "${summary_file}"
 jq '.[] | .[0] + " " + .[1]' -r <mirrors.json | while read -r mirror_url mirror_name; do
-	curl -L "$mirror_url/$filename" >"$temp_path"
+	curl -w "%{time_total}" -L "$mirror_url/$filename" -o "$temp_path" > "${time_file}"
 	download_status="$?"
+    echo "| ${mirror_name} | ${mirror_url} | $(cat ${time_file}) |" >> "${summary_file}"
 	mirror_sum="$(sha512sum "$temp_path")"
 
 	if [ "$download_status" -eq 0 ]; then
@@ -25,4 +29,4 @@ jq '.[] | .[0] + " " + .[1]' -r <mirrors.json | while read -r mirror_url mirror_
 		https://api.pushover.net/1/messages.json
 done
 
-rm "$temp_path"
+rm "$temp_path" "$time_file"
