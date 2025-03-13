ec0a553055dc11792ba3f8acb39bb09354c3964e
jiacai2050 <dev@liujiacai.net>
2025-03-13 10:50:17 +0800

    update summary file location

--- check-mirrors.sh
+++ check-mirrors.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 temp_path="$(mktemp)"
-summary_file=${GITHUB_STEP_SUMMARY:-/tmp/check-mirror-summary.md}
+summary_file=${GITHUB_STEP_SUMMARY:-check-mirror-summary.md}
 time_file="$(mktemp)"
 zig_version="$(curl -sSL 'https://ziglang.org/download/index.json' | jq -r '.master.version')"
 filename="zig-linux-x86_64-$zig_version.tar.xz"
@@ -12,7 +12,7 @@ correct_sum="$(sha512sum "$temp_path")"
 echo "| Mirror | URL | Cost(seconds) |" >> "${summary_file}"
 echo "|:-:|:-:|:-:|" >> "${summary_file}"
 jq '.[] | .[0] + " " + .[1]' -r <mirrors.json | while read -r mirror_url mirror_name; do
-	curl -w "%{time_total}" -L "$mirror_url/$filename" -o "$temp_path" > "${time_file}"
+	curl -w "%{time_total}" -L "$mirror_url/$filename" -o "$temp_path" >"${time_file}"
 	download_status="$?"
 	echo "| ${mirror_name} | ${mirror_url} | $(cat ${time_file}) |" >>"${summary_file}"
 	mirror_sum="$(sha512sum "$temp_path")"
