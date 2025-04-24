d9d5fe7a99af5ad573f9939a30faf50e1d297967
Francesco Furlan <francesco.furlan@sysdig.com>
2025-04-24 11:03:44 +0200

    chore(node_modules): upgrade @actions/cache to v4 and npm update

--- node_modules/bare-addon-resolve/lib/errors.d.ts
+++ node_modules/bare-addon-resolve/lib/errors.d.ts
@@ -2,6 +2,7 @@ declare class AddonResolveError extends Error {
   readonly code: string
 
   static INVALID_ADDON_SPECIFIER(msg: string): AddonResolveError
+  static INVALID_PACKAGE_NAME(msg: string): AddonResolveError
 }
 
 export = AddonResolveError
