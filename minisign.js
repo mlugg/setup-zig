9d38a7af8e8c3b19855b3596d164a6bedf3f2885
mlugg <mlugg@mlugg.co.uk>
2025-05-04 07:00:16 +0100

    minisign: fix typo

--- minisign.js
+++ minisign.js
@@ -50,7 +50,7 @@ function parseSignature(sig_buf) {
   sig_buf = sig_buf.subarray(trusted_header.byteLength);
 
   // Read and skip trusted comment
-  const trusted_comment_end = sig_bug.indexOf('\n');
+  const trusted_comment_end = sig_buf.indexOf('\n');
   const trusted_comment = sig_buf.subarray(0, trusted_comment_end);
   sig_buf = sig_buf.subarray(trusted_comment_end + 1);
 
@@ -70,7 +70,7 @@ function parseSignature(sig_buf) {
     key_id: key_id,
     signature: signature,
     trusted_comment: trusted_comment,
-    global_signature: global_signature,
+    global_signature: global_sig,
   };
 }
 
